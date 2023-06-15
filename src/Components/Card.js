54dc2be84151e201351c9840208fce01be36aa06
Mindru-Bogdan <77194184+Mindru-Bogdan@users.noreply.github.com>
2023-06-15 17:31:07 +0300

    Device Friendly Solution

--- src/Components/Card.js
+++ src/Components/Card.js
@@ -3,28 +3,36 @@ import classes from './Card.module.css';
 const Card = () => {
   return (
     <div>
-      <div>
-        <h1>Join our community</h1>
-        <p>30-day, hassle-free money back guarantee</p>
-        <p>
-          Gain access to our full library of tutorials along with expert code reviews. Perfect for any developers who are
-          serious about honing their skills.
-        </p>
+      <div className={classes.card}>
+        <div className={classes.join}>
+          <h1>Join our community</h1>
+          <p className={classes.yellow}>30-day, hassle-free money back guarantee</p>
+          <p className={classes.description}>
+            Gain access to our full library of tutorials along with expert code reviews. Perfect for any developers who are
+            serious about honing their skills.
+          </p>
+        </div>
+        <div className={classes.payment}>
+          <h2>Monthly Subscription</h2>
+          <p className={classes.value}>$29 </p>
+          <p className={classes.month}>per month</p>
+          <p className={classes.access}>Full access for less than $1 a day</p>
+          <p className={classes.signUp}>Sign Up</p>
+        </div>
+        <div className={classes.about}>
+          <h3>Why Us</h3>
+          <ul className={classes.aboutUs}>
+            <li>Tutorials by industry experts</li>
+            <li>Peer & expert code review</li>
+            <li>Coding exercises</li>
+            <li>Access to our GitHub repos</li>
+            <li>Community forum</li>
+            <li>Flashcard decks</li>
+            <li>New videos every week</li>
+          </ul>
+        </div>
       </div>
-      <div>
-        <h2>Monthly Subscription</h2>
-        <p> $ 29 </p>
-        <p>per month</p>
-        <p>Full access for less than $ 1 a day</p>
-        <button>Sign Up</button>
-      </div>
-      <div>
-        <h3>Why Us</h3>
-        <p>
-          Tutorials by industry experts Peer & expert code review Coding exercises Access to our GitHub repos Community forum
-          Flashcard decks New videos every week
-        </p>
-      </div>
+      <div className={classes.whiteSpace} />
     </div>
   );
 };
